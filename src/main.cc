8dbccd5bf61d9c2d88e7f14b052d9c465514f0e9
Alexey <lavrik-aleksey-1@yandex.ru>
2026-02-23 22:48:47 +0300

    implement main.cc

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,53 @@
+#include <fstream>
 #include <iostream>
+#include <stdexcept>
+#include <string>
 
-int main() {
+#include "onnx_loader.hpp"
+#include "parse_cli.hpp"
 
+static void PrintGraphSummary(const onnx::ModelProto& model) {
+    const auto& g = model.graph();
 
-    return 0;
+    std::cout << "Graph name: " << g.name() << "\n";
+    std::cout << "Inputs: " << g.input_size()
+              << " Outputs: " << g.output_size()
+              << " Initializers: " << g.initializer_size()
+              << " Nodes: " << g.node_size() << "\n\n";
+
+    for (int i = 0; i < g.node_size(); ++i) {
+        const auto& n = g.node(i);
+
+        std::cout << "Node #" << i << ": op_type=" << n.op_type();
+        if (!n.name().empty()) std::cout << " name=" << n.name();
+        std::cout << "\n  inputs: ";
+        for (const auto& in : n.input()) std::cout << in << " ";
+        std::cout << "\n  outputs: ";
+        for (const auto& out : n.output()) std::cout << out << " ";
+        std::cout << "\n";
+
+        for (const auto& a : n.attribute()) {
+            std::cout << "  attr: " << a.name() << " type=" << a.type() << "\n";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main(int argc, const char** argv) {
+    try {
+        auto [program, cfg] = tensor_compiler::ParseCli(argc, argv);
+        if (program.mode == tensor_compiler::CliMode::kExit) {
+            std::cout << program.exit_action.exit_text;
+            return program.exit_action.exit_code;
+        }
+
+        onnx::ModelProto model = tensor_compiler::LoadOnnxModel(cfg.value().onnx_filename);
+
+        PrintGraphSummary(model);
+
+        return 0;
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << "\n";
+        return 1;
+    }
 }
