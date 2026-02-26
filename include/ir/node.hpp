0bc46b696b2d06a4d6e448bbb22186aa40e3045f
Alexey <lavrik-aleksey-1@yandex.ru>
2026-02-26 11:45:18 +0300

    implement class node

---
+++ include/ir/node.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+#include "attr.hpp"
+#include "types.hpp"
+
+namespace tensor_compiler::ir {
+
+enum class OpCode {
+    kUnknown = 0,
+    kAdd = 1,
+    kMul = 2,
+    kConv = 3,
+    kRelu = 4,
+    kMatMul = 5,
+    kGemm = 6,
+};
+
+inline OpCode ParseOpCode(std::string_view op) noexcept {
+    if (op == "Add") return OpCode::kAdd;
+    if (op == "Mul") return OpCode::kMul;
+    if (op == "Conv") return OpCode::kConv;
+    if (op == "Relu") return OpCode::kRelu;
+    if (op == "MatMul") return OpCode::kMatMul;
+    if (op == "Gemm") return OpCode::kGemm;
+    return OpCode::kUnknown;
+}
+
+struct Node final {
+    std::string name;
+
+    std::string op_name;
+    OpCode op;
+
+    std::vector<ValueId> inputs;
+    std::vector<ValueId> outputs;
+
+    AttrMap attrs;
+
+    Node(std::string op_name_,
+         std::vector<ValueId> in = {},
+         std::vector<ValueId> out = {},
+         std::string name_ = {})
+        : name(std::move(name_)),
+          op_name(std::move(op_name_)),
+          op(ParseOpCode(op_name)),
+          inputs(std::move(in)),
+          outputs(std::move(out))
+    {}
+};
+
+} // namespace tensor_compiler::ir
